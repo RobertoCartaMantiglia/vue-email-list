61daabede09a92e91828bd77543d16b061f23feb
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2022-12-21 15:33:38 +0100

    add axios

--- js/script.js
+++ js/script.js
@@ -15,3 +15,10 @@ createApp({
     }
   }
 }).mount('#app')
+
+
+axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then( function(response){
+        const result = response.data;
+        console.log(result)
+    })
